5bffce6b5ee0e68c4e17e9f23e04c5d5131eb04b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-08 10:51:56 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/countdiv/CountDiv.tsx
+++ src/countdiv/CountDiv.tsx
@@ -6,16 +6,16 @@ const countdiv = () => {
             <div className="bg-gray-900 rounded px-8 py-4 my-8">
                 <h1 className="text-2xl font-semibold text-center">Title</h1>
             </div>
-            <div className="bg-gray-900 rounded px-8 py-4">
+            <div className="bg-gray-900 rounded px-8 py-4 my-8">
                 <h1 className="text-2xl font-semibold text-center">Title</h1>
             </div>
-            <div className="bg-gray-900 rounded px-8 py-4">
+            <div className="bg-gray-900 rounded px-8 py-4 my-8">
                 <h1 className="text-2xl font-semibold text-center">Title</h1>
             </div> 
-            <div className="bg-gray-900 rounded px-8 py-4">
+            <div className="bg-gray-900 rounded px-8 py-4 my-8">
                 <h1 className="text-2xl font-semibold text-center">Title</h1>
             </div>
-            <div className="bg-gray-900 rounded px-8 py-4">
+            <div className="bg-gray-900 rounded px-8 py-4 my-8">
                 <h1 className="text-2xl font-semibold text-center">Title</h1>
             </div>
         </div>
