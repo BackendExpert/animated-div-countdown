f1dc22937da9cb155541c3dd4d40db6a1c099613
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-08 11:12:49 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/countdiv/CountDiv.tsx
+++ src/countdiv/CountDiv.tsx
@@ -7,7 +7,7 @@ const countdiv = () => {
         <div className="md:grid md:grid-cols-5 md:gap-4 text-white md:px-8 md:my-8">
             <div className="bg-gray-900 rounded-xl px-8 py-4 my-8 md:my-0">
                 <h1 className="text-2xl font-semibold text-center">Patient</h1>
-                <span className="text-center"><IonIcon name="newspaper" /></span>
+                <span><IonIcon size="large" name="newspaper" /></span>
             </div>
             <div className="bg-gray-900 rounded-xl px-8 py-4 my-8 md:my-0">
                 <h1 className="text-2xl font-semibold text-center">Research</h1>
