d47184e5bcdeed4799b3edda6c72f8fb94b442a8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-08 10:42:51 +0530

    Updaet
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/countdiv/CountDiv.tsx
+++ src/countdiv/CountDiv.tsx
@@ -1,7 +1,7 @@
 
 const countdiv = () => {
   return (
-    <div className="grid grid-cols-5 gap-4">
+    <div className="grid grid-cols-5 gap-4 px-8">
         <div>Div1</div>
         <div>Div1</div>
         <div>Div1</div>
