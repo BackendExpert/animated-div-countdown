ef4f35f1016fe3ad33c903a8ea7afb1ebcd4bc46
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-08 11:47:33 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/countdiv/CountDiv.tsx
+++ src/countdiv/CountDiv.tsx
@@ -27,7 +27,7 @@ const countdiv = () => {
                 </p>
             </div> 
             <div className="bg-gray-900 rounded-xl px-8 py-4 my-8 md:my-0">
-                <span className="px-[60px]"><IonIcon size="large" name="business" /></span>
+                <span className="px-[60px] content-center"><IonIcon size="large" name="business" /></span>
                 <h1 className="text-2xl font-thin text-center pt-4 pb-0">Faculties</h1>
                 <p className="text-xl text-center pt-0 pb-2 font-semibold">
                     <CountUp end={8} />
