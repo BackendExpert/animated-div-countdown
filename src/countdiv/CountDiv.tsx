c35e981e87b839604856498e212494df195e0017
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-08 11:26:16 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/countdiv/CountDiv.tsx
+++ src/countdiv/CountDiv.tsx
@@ -6,20 +6,29 @@ const countdiv = () => {
     <div className="container mx-auto px-12">
         <div className="md:grid md:grid-cols-5 md:gap-4 text-white md:px-8 md:my-8">
             <div className="bg-gray-900 rounded-xl px-8 py-4 my-8 md:my-0">
-                <h1 className="text-2xl font-semibold text-center">Patient</h1>
-                <span><IonIcon size="large" name="newspaper" /></span>
+                <span className="px-[60px]"><IonIcon size="large" name="newspaper" /></span>
+                <h1 className="text-2xl font-thin text-center pt-4 pb-0">Patient</h1>
+                <p className="text-xl text-center pt-0 pb-2 font-semibold">100</p>
             </div>
             <div className="bg-gray-900 rounded-xl px-8 py-4 my-8 md:my-0">
-                <h1 className="text-2xl font-semibold text-center">Research</h1>
+                <span className="px-[60px]"><IonIcon size="large" name="newspaper" /></span>
+                <h1 className="text-2xl font-thin text-center pt-4 pb-0">Research</h1>
+                <p className="text-xl text-center pt-0 pb-2 font-semibold">100</p>
             </div>
             <div className="bg-gray-900 rounded-xl px-8 py-4 my-8 md:my-0">
-                <h1 className="text-2xl font-semibold text-center">Peoples</h1>
+                <span className="px-[60px]"><IonIcon size="large" name="newspaper" /></span>
+                <h1 className="text-2xl font-thin text-center pt-4 pb-0">People</h1>
+                <p className="text-xl text-center pt-0 pb-2 font-semibold">100</p>
             </div> 
             <div className="bg-gray-900 rounded-xl px-8 py-4 my-8 md:my-0">
-                <h1 className="text-2xl font-semibold text-center">Faculties</h1>
+                <span className="px-[60px]"><IonIcon size="large" name="newspaper" /></span>
+                <h1 className="text-2xl font-thin text-center pt-4 pb-0">Patient</h1>
+                <p className="text-xl text-center pt-0 pb-2 font-semibold">100</p>
             </div>
             <div className="bg-gray-900 rounded-xl px-8 py-4 my-8 md:my-0">
-                <h1 className="text-2xl font-semibold text-center">Views</h1>
+                <span className="px-[60px]"><IonIcon size="large" name="newspaper" /></span>
+                <h1 className="text-2xl font-thin text-center pt-4 pb-0">Patient</h1>
+                <p className="text-xl text-center pt-0 pb-2 font-semibold">100</p>
             </div>
         </div>
     </div>
