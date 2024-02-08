bec3b600ba856b8311dbb9713c07c957a4f7c8bf
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-08 10:51:32 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/countdiv/CountDiv.tsx
+++ src/countdiv/CountDiv.tsx
@@ -3,8 +3,8 @@ const countdiv = () => {
   return (
     <div className="container mx-auto px-12">
         <div className="md:grid md:grid-cols-5 md:gap-4 text-white md:px-8 md:my-8">
-            <div className="bg-gray-900 rounded px-8 py-4">
-                <h1 className="text-2xl font-semibold text-center my-10">Title</h1>
+            <div className="bg-gray-900 rounded px-8 py-4 my-8">
+                <h1 className="text-2xl font-semibold text-center">Title</h1>
             </div>
             <div className="bg-gray-900 rounded px-8 py-4">
                 <h1 className="text-2xl font-semibold text-center">Title</h1>
