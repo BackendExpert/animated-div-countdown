cebfdf1fec7b41f3fedd55606efd7f97192be958
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-08 10:48:56 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/countdiv/CountDiv.tsx
+++ src/countdiv/CountDiv.tsx
@@ -2,21 +2,21 @@
 const countdiv = () => {
   return (
     <div className="container mx-auto px-12">
-        <div className="grid grid-cols-5 gap-4 text-white px-8">
+        <div className="grid grid-cols-5 gap-4 text-white px-8 my-8">
             <div className="bg-gray-900 rounded px-8 py-4">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic atque adipisci sit, illo quo quia, fugiat eum enim, ea dolores ipsam error assumenda animi. Molestiae repudiandae molestias voluptas eaque cupiditate.
+                <h1 className="text-2xl font-semibold text-center">Title</h1>
             </div>
             <div className="bg-gray-900 rounded px-8 py-4">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic atque adipisci sit, illo quo quia, fugiat eum enim, ea dolores ipsam error assumenda animi. Molestiae repudiandae molestias voluptas eaque cupiditate.
+                <h1 className="text-2xl font-semibold text-center">Title</h1>
             </div>
             <div className="bg-gray-900 rounded px-8 py-4">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic atque adipisci sit, illo quo quia, fugiat eum enim, ea dolores ipsam error assumenda animi. Molestiae repudiandae molestias voluptas eaque cupiditate.
-            </div>   
+                <h1 className="text-2xl font-semibold text-center">Title</h1>
+            </div> 
             <div className="bg-gray-900 rounded px-8 py-4">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic atque adipisci sit, illo quo quia, fugiat eum enim, ea dolores ipsam error assumenda animi. Molestiae repudiandae molestias voluptas eaque cupiditate.
+                <h1 className="text-2xl font-semibold text-center">Title</h1>
             </div>
             <div className="bg-gray-900 rounded px-8 py-4">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic atque adipisci sit, illo quo quia, fugiat eum enim, ea dolores ipsam error assumenda animi. Molestiae repudiandae molestias voluptas eaque cupiditate.
+                <h1 className="text-2xl font-semibold text-center">Title</h1>
             </div>
         </div>
     </div>
