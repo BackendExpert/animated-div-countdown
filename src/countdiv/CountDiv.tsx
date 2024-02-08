44f310482efaea128869f6d499755e8959fbac1c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-08 11:35:32 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/countdiv/CountDiv.tsx
+++ src/countdiv/CountDiv.tsx
@@ -8,27 +8,37 @@ const countdiv = () => {
             <div className="bg-gray-900 rounded-xl px-8 py-4 my-8 md:my-0">
                 <span className="px-[60px]"><IonIcon size="large" name="newspaper" /></span>
                 <h1 className="text-2xl font-thin text-center pt-4 pb-0">Patient</h1>
-                <p className="text-xl text-center pt-0 pb-2 font-semibold">100</p>
+                <p className="text-xl text-center pt-0 pb-2 font-semibold">
+                    <CountUp end={12} />
+                </p>
             </div>
             <div className="bg-gray-900 rounded-xl px-8 py-4 my-8 md:my-0">
                 <span className="px-[60px]"><IonIcon size="large" name="telescope" /></span>
                 <h1 className="text-2xl font-thin text-center pt-4 pb-0">Research</h1>
-                <p className="text-xl text-center pt-0 pb-2 font-semibold">100</p>
+                <p className="text-xl text-center pt-0 pb-2 font-semibold">
+                    <CountUp end={54} />
+                </p>
             </div>
             <div className="bg-gray-900 rounded-xl px-8 py-4 my-8 md:my-0">
                 <span className="px-[60px]"><IonIcon size="large" name="people" /></span>
                 <h1 className="text-2xl font-thin text-center pt-4 pb-0">People</h1>
-                <p className="text-xl text-center pt-0 pb-2 font-semibold">100</p>
+                <p className="text-xl text-center pt-0 pb-2 font-semibold">
+                    <CountUp end={450} />
+                </p>
             </div> 
             <div className="bg-gray-900 rounded-xl px-8 py-4 my-8 md:my-0">
                 <span className="px-[60px]"><IonIcon size="large" name="business" /></span>
                 <h1 className="text-2xl font-thin text-center pt-4 pb-0">Faculties</h1>
-                <p className="text-xl text-center pt-0 pb-2 font-semibold">100</p>
+                <p className="text-xl text-center pt-0 pb-2 font-semibold">
+                    <CountUp end={8} />
+                </p>
             </div>
             <div className="bg-gray-900 rounded-xl px-8 py-4 my-8 md:my-0">
                 <span className="px-[60px]"><IonIcon size="large" name="eye" /></span>
                 <h1 className="text-2xl font-thin text-center pt-4 pb-0">Views</h1>
-                <p className="text-xl text-center pt-0 pb-2 font-semibold">100</p>
+                <p className="text-xl text-center pt-0 pb-2 font-semibold">
+                    <CountUp end={1000} />
+                </p>
             </div>
         </div>
     </div>
