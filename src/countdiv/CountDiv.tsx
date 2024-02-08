55a12547df16d3c0b72e71f301c20018f0db5de9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-08 10:47:02 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/countdiv/CountDiv.tsx
+++ src/countdiv/CountDiv.tsx
@@ -1,13 +1,23 @@
 
 const countdiv = () => {
   return (
-    <div className="">
-        <div className="grid grid-cols-5 gap-4">
-            <div>Div1</div>
-            <div>Div1</div>
-            <div>Div1</div>
-            <div>Div1</div>
-            <div>Div1</div>       
+    <div className="container mx-auto px-12">
+        <div className="grid grid-cols-5 gap-4 text-white px-8">
+            <div className="bg-gray-900 rounded px-8 py-4">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic atque adipisci sit, illo quo quia, fugiat eum enim, ea dolores ipsam error assumenda animi. Molestiae repudiandae molestias voluptas eaque cupiditate.
+            </div>
+            <div className="bg-gray-900 rounded px-8 py-4">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic atque adipisci sit, illo quo quia, fugiat eum enim, ea dolores ipsam error assumenda animi. Molestiae repudiandae molestias voluptas eaque cupiditate.
+            </div>
+            <div className="bg-gray-900 rounded px-8 py-4">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic atque adipisci sit, illo quo quia, fugiat eum enim, ea dolores ipsam error assumenda animi. Molestiae repudiandae molestias voluptas eaque cupiditate.
+            </div>   
+            <div className="bg-gray-900 rounded px-8 py-4">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic atque adipisci sit, illo quo quia, fugiat eum enim, ea dolores ipsam error assumenda animi. Molestiae repudiandae molestias voluptas eaque cupiditate.
+            </div>
+            <div className="bg-gray-900 rounded px-8 py-4">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic atque adipisci sit, illo quo quia, fugiat eum enim, ea dolores ipsam error assumenda animi. Molestiae repudiandae molestias voluptas eaque cupiditate.
+            </div>
         </div>
     </div>
   )
